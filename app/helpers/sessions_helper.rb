f71498c7e65c38b5a305fc214c2717d3740e3a26
shogoT <kurohamu2525@gmail.com>
2023-02-21 23:43:35 +0900

    ユーザー情報編集の認可機能追加

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -38,6 +38,11 @@ module SessionsHelper
     end
   end
 
+  # 渡されたユーザーがログイン済みのユーザーであればtrueを返します。
+  def current_user?(user)
+    user == current_user
+  end
+
   def logged_in?
     !current_user.nil?
   end
